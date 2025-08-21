21f393a4243e070e20a831424ba0fd2ba6f44ed0
Niklas Roth <nikicello@gmail.com>
2025-08-21 21:41:40 +0200

    initial LED is blinking

--- src/main.c
+++ src/main.c
@@ -1,6 +1,30 @@
-#include <zephyr/kernel.h>
 
-int main(void)
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+
+#define LED0_NODE DT_ALIAS(led0)
+
+#if !DT_NODE_HAS_STATUS(LED0_NODE, okay)
+#error "Unsupported board: led0 devicetree alias is not defined"
+#endif
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+void main(void)
 {
-        return 0;
+        int ret;
+
+        if (!device_is_ready(led.port)) {
+                return;
+        }
+
+        ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+        if (ret < 0) {
+                return;
+        }
+
+        while (1) {
+                gpio_pin_toggle_dt(&led);
+                k_msleep(500);
+        }
 }
